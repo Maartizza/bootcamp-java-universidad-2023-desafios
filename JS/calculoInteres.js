e40461fd8ad9a8a11348141972e68c3d05c3ff92
Maartizza <132380021+Maartizza@users.noreply.github.com>
2023-08-16 11:54:08 -0300

    Add files via upload

---
+++ JS/calculoInteres.js
@@ -0,0 +1,60 @@
+let planes = [
+    {
+      nombre: "Plan 001",
+      capital: 150000,
+      dias: 30,
+      tasa: 15/100
+    },
+    {
+      nombre: "Plan 002",
+      capital: 300000,
+      dias: 180,
+      tasa: 10/100
+    },
+    {
+      nombre: "Plan 003",
+      capital: 485000,
+      dias: 60,
+      tasa: 23/100
+    }
+      
+  ];
+
+
+let calculoInteres = planes.map(planes => {
+    let calculoGenerado = (planes.capital*planes.dias*planes.tasa)/360;
+        document.writeln(`<br/> Nombre: ${planes.nombre} Interes Generado: $ ${calculoGenerado} `);
+    return calculoGenerado;
+});
+
+document.writeln('<br/> ------------------');
+
+
+let nuevoPlanes = planes.map (planes =>{
+  return{
+    ...planes,
+    interes: ((planes.capital*planes.dias*planes.tasa)/360)
+    } 
+});
+
+
+
+
+
+
+
+/* document.writeln(`<br/> Nombre: ${nuevoPlanes.nombre}
+   Capital: $ ${nuevoPlanes.capital}
+   Dias: ${nuevoPlanes.dias}
+   Tasa: ${nuevoPlanes.tasa} anual
+   Interes ${nuevoPlanes.interes} ` ) */
+  
+
+console.log (calculoInteres);
+
+console.log (nuevoPlanes);
+
+document.writeln(`<br/> ${nuevoPlanes}`);
+
+
+
